1270c96707437d8e8c9f248cb187231739c7f782
daniil148852 <daniildan850@gmail.com>
2026-01-28 16:25:55 +0100

    Update window.c

--- gui/window.c
+++ gui/window.c
@@ -10,32 +10,25 @@ void window_init(struct window* win, int x, int y, int w, int h, const char* tit
     strncpy(win->title, title, 31);
     win->visible = 1;
     win->active = 0;
-    win->draw = (void*)0;
-    win->on_click = (void*)0;
+    win->draw = NULL;
+    win->on_click = NULL;
 }
 
 void window_draw(struct window* win) {
     if (!win->visible) return;
     
-    // Shadow
     vga_fillrect(win->x + 3, win->y + 3, win->w, win->h, COLOR_DGRAY);
-    
-    // Window body
     vga_fillrect(win->x, win->y, win->w, win->h, COLOR_LGRAY);
     
-    // Title bar
     uint8_t title_color = win->active ? COLOR_BLUE : COLOR_DGRAY;
     vga_fillrect(win->x, win->y, win->w, 12, title_color);
     vga_print(win->x + 4, win->y + 2, win->title, COLOR_WHITE);
     
-    // Close button
     vga_fillrect(win->x + win->w - 12, win->y + 1, 10, 10, COLOR_RED);
     vga_print(win->x + win->w - 10, win->y + 2, "X", COLOR_WHITE);
     
-    // Border
     vga_rect(win->x, win->y, win->w, win->h, COLOR_BLACK);
     
-    // Custom draw
     if (win->draw) {
         win->draw(win);
     }
