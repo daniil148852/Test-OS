1dfd7d2b56503af5347b3b2c3fecf993668e532c
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:34:59 +0100

    Update timer.c

--- drivers/timer.c
+++ drivers/timer.c
@@ -1,1 +1,21 @@
+#include "drivers/timer.h"
+#include "drivers/ports.h"
 
+static volatile uint32_t ticks = 0;
+
+void timer_init(void) {
+    uint32_t divisor = 1193180 / 100;
+    
+    outb(0x43, 0x36);
+    outb(0x40, divisor & 0xFF);
+    outb(0x40, (divisor >> 8) & 0xFF);
+}
+
+void timer_handler(void) {
+    ticks++;
+    outb(0x20, 0x20);
+}
+
+uint32_t timer_get_ticks(void) {
+    return ticks;
+}
