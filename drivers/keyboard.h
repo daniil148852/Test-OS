acc443dcc0ce800d25cf9dd3732f2513dceaafa9
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:32:59 +0100

    Update keyboard.h

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,1 +1,11 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
 
+#include <stdint.h>
+
+void keyboard_init(void);
+void keyboard_handler(void);
+char keyboard_getchar(void);
+int keyboard_haskey(void);
+
+#endif
