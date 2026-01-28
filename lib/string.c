ed35e2c65c6186278924c14a561713b61a5982bb
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:36:01 +0100

    Update string.c

--- lib/string.c
+++ lib/string.c
@@ -1,1 +1,67 @@
+#include "lib/string.h"
 
+void* memset(void* ptr, int value, size_t num) {
+    uint8_t* p = (uint8_t*)ptr;
+    while (num--) *p++ = (uint8_t)value;
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t num) {
+    uint8_t* d = (uint8_t*)dest;
+    const uint8_t* s = (const uint8_t*)src;
+    while (num--) *d++ = *s++;
+    return dest;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(uint8_t*)s1 - *(uint8_t*)s2;
+}
+
+char* strncpy(char* dest, const char* src, size_t n) {
+    size_t i;
+    for (i = 0; i < n && src[i]; i++) {
+        dest[i] = src[i];
+    }
+    for (; i < n; i++) {
+        dest[i] = '\0';
+    }
+    return dest;
+}
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (*str++) len++;
+    return len;
+}
+
+char* strcpy(char* dest, const char* src) {
+    char* d = dest;
+    while ((*d++ = *src++));
+    return dest;
+}
+
+void itoa(int num, char* str) {
+    int i = 0;
+    int sign = num;
+    
+    if (sign < 0) num = -num;
+    
+    do {
+        str[i++] = num % 10 + '0';
+    } while ((num /= 10) > 0);
+    
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
+    
+    // Reverse
+    int j;
+    for (j = 0; j < i / 2; j++) {
+        char tmp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = tmp;
+    }
+}
