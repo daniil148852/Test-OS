2880241b93d03bf492eab8941aed9276c8300f24
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:30:36 +0100

    Update irq.c

--- kernel/irq.c
+++ kernel/irq.c
@@ -1,1 +1,30 @@
+#include "kernel/irq.h"
+#include "kernel/idt.h"
+#include "drivers/ports.h"
 
+extern void irq0(void);
+extern void irq1(void);
+extern void irq12(void);
+
+void irq_remap(void) {
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+}
+
+void irq_init(void) {
+    irq_remap();
+    
+    idt_set_gate(32, (uint32_t)irq0, 0x08, 0x8E);
+    idt_set_gate(33, (uint32_t)irq1, 0x08, 0x8E);
+    idt_set_gate(44, (uint32_t)irq12, 0x08, 0x8E);
+    
+    __asm__ volatile("sti");
+}
