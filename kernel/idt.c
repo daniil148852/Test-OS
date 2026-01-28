943aeb67d9434f193bec734f8c1f3b7a64a1862a
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:30:05 +0100

    Update idt.c

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,1 +1,24 @@
+#include "kernel/idt.h"
+#include "lib/string.h"
 
+extern void idt_load(uint32_t);
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags) {
+    idt[num].base_lo = base & 0xFFFF;
+    idt[num].base_hi = (base >> 16) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].always0 = 0;
+    idt[num].flags = flags;
+}
+
+void idt_init(void) {
+    idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+    idtp.base = (uint32_t)&idt;
+
+    memset(&idt, 0, sizeof(struct idt_entry) * 256);
+
+    idt_load((uint32_t)&idtp);
+}
