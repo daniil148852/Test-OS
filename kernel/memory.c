53c9facae15cfc8544d1ad837a1bf7cdda47c48b
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:31:11 +0100

    Update memory.c

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,1 +1,26 @@
+#include "kernel/memory.h"
 
+#define HEAP_START 0x200000
+#define HEAP_SIZE  0x100000
+
+static uint8_t* heap_ptr = (uint8_t*)HEAP_START;
+static uint8_t* heap_end = (uint8_t*)(HEAP_START + HEAP_SIZE);
+
+void memory_init(void) {
+    heap_ptr = (uint8_t*)HEAP_START;
+}
+
+void* kmalloc(size_t size) {
+    if (heap_ptr + size > heap_end) {
+        return (void*)0;
+    }
+    void* ptr = heap_ptr;
+    heap_ptr += size;
+    // Align to 4 bytes
+    heap_ptr = (uint8_t*)(((uint32_t)heap_ptr + 3) & ~3);
+    return ptr;
+}
+
+void kfree(void* ptr) {
+    (void)ptr; // Simple allocator - no free
+}
