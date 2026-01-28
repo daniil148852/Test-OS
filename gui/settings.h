23a27fd6522af2bf726c3462fa2bcd43779eb0e6
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:39:50 +0100

    Update settings.h

--- gui/settings.h
+++ gui/settings.h
@@ -1,1 +1,10 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
 
+#include "gui/window.h"
+
+void settings_init(struct window* win);
+void settings_draw(struct window* win);
+void settings_click(struct window* win, int x, int y);
+
+#endif
