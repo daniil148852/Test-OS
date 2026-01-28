b12481ab833a405ef9a913885dda62f89f2f7ee4
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:36:25 +0100

    Update gui.h

--- gui/gui.h
+++ gui/gui.h
@@ -1,1 +1,16 @@
+#ifndef GUI_H
+#define GUI_H
 
+#include <stdint.h>
+
+#define GUI_STATE_WELCOME  0
+#define GUI_STATE_SETUP    1
+#define GUI_STATE_DESKTOP  2
+#define GUI_STATE_SETTINGS 3
+#define GUI_STATE_FILEMAN  4
+
+void gui_set_state(int state);
+int gui_get_state(void);
+void gui_draw_cursor(int x, int y);
+
+#endif
