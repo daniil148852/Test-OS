1414c17054f7ceed6eb77a076aa6cb08b8ce1231
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:33:35 +0100

    Update mouse.h

--- drivers/mouse.h
+++ drivers/mouse.h
@@ -1,1 +1,12 @@
+#ifndef MOUSE_H
+#define MOUSE_H
 
+#include <stdint.h>
+
+void mouse_init(void);
+void mouse_handler(void);
+int mouse_get_x(void);
+int mouse_get_y(void);
+int mouse_get_buttons(void);
+
+#endif
