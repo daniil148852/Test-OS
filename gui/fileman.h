a4f9d0b8bd889a256960a5380992b90a71918af5
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:40:17 +0100

    Update fileman.h

--- gui/fileman.h
+++ gui/fileman.h
@@ -1,1 +1,10 @@
+#ifndef FILEMAN_H
+#define FILEMAN_H
 
+#include "gui/window.h"
+
+void fileman_init(struct window* win);
+void fileman_draw(struct window* win);
+void fileman_click(struct window* win, int x, int y);
+
+#endif
