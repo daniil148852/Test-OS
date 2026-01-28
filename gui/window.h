f078787ebb6a9a5c7d8d62a76d16231539df6392
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:36:58 +0100

    Update window.h

--- gui/window.h
+++ gui/window.h
@@ -1,1 +1,22 @@
+#ifndef WINDOW_H
+#define WINDOW_H
 
+#include <stdint.h>
+
+#define MAX_WINDOWS 8
+
+struct window {
+    int x, y, w, h;
+    char title[32];
+    int visible;
+    int active;
+    void (*draw)(struct window*);
+    void (*on_click)(struct window*, int, int);
+};
+
+void window_init(struct window* win, int x, int y, int w, int h, const char* title);
+void window_draw(struct window* win);
+int window_hit_test(struct window* win, int x, int y);
+int window_hit_close(struct window* win, int x, int y);
+
+#endif
