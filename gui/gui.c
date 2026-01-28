ce0b7353aab8e4738fa94b341072b18a5da821dc
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:36:42 +0100

    Update gui.c

--- gui/gui.c
+++ gui/gui.c
@@ -1,1 +1,25 @@
+#include "gui/gui.h"
+#include "drivers/vga.h"
 
+static int current_state = GUI_STATE_WELCOME;
+
+void gui_set_state(int state) {
+    current_state = state;
+}
+
+int gui_get_state(void) {
+    return current_state;
+}
+
+void gui_draw_cursor(int x, int y) {
+    // Arrow cursor
+    for (int i = 0; i < 10; i++) {
+        vga_putpixel(x, y + i, COLOR_WHITE);
+    }
+    for (int i = 0; i < 6; i++) {
+        vga_putpixel(x + i, y + i, COLOR_WHITE);
+    }
+    vga_putpixel(x + 1, y + 6, COLOR_WHITE);
+    vga_putpixel(x + 2, y + 7, COLOR_WHITE);
+    vga_putpixel(x + 3, y + 8, COLOR_WHITE);
+}
