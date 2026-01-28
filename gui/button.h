d6bf9bc7faec8909b0330d7c7ebfde2b035caffe
daniil148852 <daniildan850@gmail.com>
2026-01-28 15:38:09 +0100

    Update button.h

--- gui/button.h
+++ gui/button.h
@@ -1,1 +1,17 @@
+#ifndef BUTTON_H
+#define BUTTON_H
 
+#include <stdint.h>
+
+struct button {
+    int x, y, w, h;
+    char label[24];
+    int pressed;
+    void (*on_click)(void);
+};
+
+void button_init(struct button* btn, int x, int y, int w, int h, const char* label, void (*callback)(void));
+void button_draw(struct button* btn);
+int button_hit_test(struct button* btn, int x, int y);
+
+#endif
